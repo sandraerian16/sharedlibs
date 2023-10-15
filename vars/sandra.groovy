82817abfd9d3c29b5608436b0432cc5744333769
sandraerian16 <36929017+sandraerian16@users.noreply.github.com>
2023-10-15 15:46:57 +0300

    Update sandra.groovy

--- vars/sandra.groovy
+++ vars/sandra.groovy
@@ -1,3 +1,21 @@
 def printHello(def name){
     println("hello ${name}")
 }
+
+def appendMessageToGitHubFile(String repoUrl, String branch, String filePath, String message) {
+    def tempWorkspace = checkout scm
+
+    // Construct the file path in the workspace
+    def workspaceFilePath = "${tempWorkspace}/${filePath}"
+
+    // Append the message to the file
+    writeFile file: workspaceFilePath, text: "${readFile(workspaceFilePath)}\n${message}"
+
+    // Commit and push the changes
+    sh """
+        cd ${tempWorkspace}
+        git add ${filePath}
+        git commit -m "Append message to ${filePath}"
+        git push origin ${branch}
+    """
+}
