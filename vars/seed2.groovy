5190183fe49e8dcf0de99ff08b1119edab76752a
sandraerian16 <36929017+sandraerian16@users.noreply.github.com>
2024-05-12 17:14:35 +0300

    Update seed2.groovy

--- vars/seed2.groovy
+++ vars/seed2.groovy
@@ -1,7 +1,14 @@
 pipelineJob("dslJob2") {
     definition {
         cps {
-            script(readFileFromWorkspace('vars/newjob.groovy'))
+           // script(readFileFromWorkspace('vars/newjob.groovy'))
+            script {
+                def params = [
+                    param1: 'value1',
+                    param2: 'value2'
+                ]
+                // Pass parameters along with the script execution
+                readFileFromWorkspace('vars/newjob.groovy').call(params)
         }
     }
 }
